56e644c04067273c8a9b3022587f6b8b8b579e0e
Iván Pereira Muñoz <elchininet@gmail.com>
2023-12-13 18:44:03 +0100

    Implement configuration

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,9 +3,12 @@ export const DESCRIPTION = 'Custom attributes for Home Assistant';
 export const URL = 'https://github.com/Mariusthvdb/custom-attributes';
 
 export enum SELECTOR {
+    HUI_VIEW = 'hui-view',
     MORE_INFO_CONTENT = 'more-info-content',
     MORE_INFO_DEFAULT = 'more-info-default',
     MORE_INFO_VACUUM = 'more-info-vacuum',
     MORE_INFO_LIGHT = 'more-info-light',
     HA_ATTRIBUTES = 'ha-attributes'
 }
+
+export const ESCAPE_REG_EXP = /[.?+^$[\]\\(){}|-]/g;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,33 @@
 export interface CustomAttributesClass {
 }
 
+export interface StateObject {
+    attributes: {
+        device_class?: string;
+    },
+    entity_id: string;
+}
+
 export interface Attributes extends Element {
-    extraFilters?: string;
+    extraFilters: string | undefined;
+    __stateObj: StateObject;
+}
+
+export interface CustomAttributesConfig {
+    filter_attributes?: {
+        by_entity_id?: Record<string, string[]>;
+        by_domain: Record<string, string[]>;
+        by_device_class?: Record<string, string[]>;
+        by_glob: Record<string, string[]>;
+    };   
+}
+
+export interface Lovelace extends HTMLElement {
+	lovelace: {
+        config: {
+            custom_attributes?: CustomAttributesConfig;
+        };
+    };
 }
 
 declare global {
