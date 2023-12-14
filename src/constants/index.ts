38eb8a37e12e64894dc016f8837ec54f7a476e15
Iván Pereira Muñoz <elchininet@gmail.com>
2023-12-14 17:42:52 +0100

    Query for the first child of the more-info content

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,10 +5,6 @@ export const URL = 'https://github.com/Mariusthvdb/custom-attributes';
 export enum SELECTOR {
     HUI_VIEW = 'hui-view',
     MORE_INFO_CONTENT = 'more-info-content',
-    MORE_INFO_DEFAULT = 'more-info-default',
-    MORE_INFO_VACUUM = 'more-info-vacuum',
-    MORE_INFO_LIGHT = 'more-info-light',
-    MORE_INFO_SIREN = 'more-info-siren',
     HA_ATTRIBUTES = 'ha-attributes'
 }
 
