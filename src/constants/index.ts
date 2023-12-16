2cb946757edf9ec77830fdb858ce967c58049122
Iván Pereira Muñoz <elchininet@gmail.com>
2023-12-16 01:21:08 +0100

    Add list of ignored attributes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -13,5 +13,25 @@ export const ESCAPE_REG_EXP = /[.?+^$[\]\\(){}|-]/g;
 export const ALL_FILTER = 'all';
 
 export const IGNORED_ATTRIBUTES = [
-    'device_class'
+    'assumed_state',
+    'attribution',
+    'custom_ui_more_info',
+    'custom_ui_state_card',
+    'device_class',
+    'editable',
+    'emulated_hue_name',
+    'emulated_hue',
+    'entity_id',
+    'entity_picture',
+    'event_types',
+    'friendly_name',
+    'haaska_hidden',
+    'haaska_name',
+    'icon',
+    'initial_state',
+    'last_reset',
+    'restored',
+    'state_class',
+    'supported_features',
+    'unit_of_measurement'
 ];
