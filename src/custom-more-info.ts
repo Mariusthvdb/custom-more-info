df179ce4513a730820f0bf9a472e45a712c5a6b4
Iván Pereira <elchininet@gmail.com>
2024-01-06 21:08:51 +0100

    Add new options to manage the header history icon (#7)
    * Add new options to manage the header history button
    
    * Refactor the name of the new options
    
    * Fix sun.sun entity

--- src/custom-more-info.ts
+++ src/custom-more-info.ts
@@ -2,8 +2,8 @@ import {
     HAQuerySelector,
     HAQuerySelectorEvent,
     OnLovelacePanelLoadDetail,
-    OnLovelaceMoreInfoDialogOpenDetail,
-    OnLovelaceHistoryAndLogBookDialogOpenDetail
+    OnMoreInfoDialogOpenDetail,
+    OnHistoryAndLogBookDialogOpenDetail
 } from 'home-assistant-query-selector';
 import {
     Lovelace,
@@ -11,7 +11,9 @@ import {
     Attributes,
     InternalFilters,
     InternalVisibility,
-    MoreInfoDialog
+    MoreInfoDialog,
+    HomeAssistant,
+    ElementsVisibility
 } from '@types';
 import {
     NAME,
@@ -25,22 +27,20 @@ import {
     addStyle,
     removeStyle,
     getHiddenStyle,
-    getPromisableElement
+    getPromisableElement,
+    getTranslations,
+    addDataSelectors
 } from '@utilities';
 import { version } from '../package.json';
-
-console.info(
-    `%c  ${NAME}  \n%c  Version ${version} ${DESCRIPTION}`,
-    'color: gold; font-weight: bold; background: black',
-    'color: white; font-weight: bold; background: steelblue'
-);
 
 class CustomMoreInfo {
 
     constructor() {
-        this._selector = new HAQuerySelector();
-        this._selector.addEventListener(HAQuerySelectorEvent.ON_PANEL_LOAD, (event) => {
-            this._debug('lovelace panel has been rendered so loading the configuration');
+        this._selector = new HAQuerySelector({
+            retries: 5,
+            delay: 500
+        });
+        this._selector.addEventListener(HAQuerySelectorEvent.ON_LOVELACE_PANEL_LOAD, (event) => {
             this.storeConfig(event.detail);
         });
         this._selector.addEventListener(HAQuerySelectorEvent.ON_MORE_INFO_DIALOG_OPEN, (event) => {
@@ -59,6 +59,7 @@ class CustomMoreInfo {
     private _config: CustomMoreInfoConfig;
     private _filters: Record<string, InternalFilters>;
     private _visibility: Record<string, InternalVisibility>;
+    private _translations: Record<string, string>;
 
     private _insertAttributesGlobs(
         entityId: string,
@@ -106,6 +107,21 @@ class CustomMoreInfo {
         });
     }
 
+    private _anyVisbilityMatch(
+        parameter: ElementsVisibility | undefined,
+        entityId: string,
+        deviceClass: string,
+        domain: string
+    ): boolean {
+        return (
+            this._anyGlobMatch(entityId, parameter?.by_glob) ||
+            parameter?.by_device_class?.includes(deviceClass) ||
+            parameter?.by_domain?.includes(domain) ||
+            parameter?.by_entity_id?.includes(entityId) ||
+            parameter?.[ALL_FILTER]
+        );
+    }
+
     private _debug(message: unknown): void {
         if (this._config?.debug) {
             if (
@@ -144,7 +160,7 @@ class CustomMoreInfo {
 
     protected storeConfig(detail: OnLovelacePanelLoadDetail): void {
         detail.HA_PANEL_LOVELACE.element
-            .then((lovelacePanel: Lovelace) => {
+            .then((lovelacePanel: Lovelace): void => {
                 const config = lovelacePanel?.lovelace?.config?.custom_more_info;
                 if (config) {
                     this._config = config;
@@ -161,10 +177,24 @@ class CustomMoreInfo {
                 this._filters = {};
                 this._visibility = {};
                 this._debug(this._config);
+            })
+            .finally(() => {
+                detail.HOME_ASSISTANT.element
+                    .then((ha: HomeAssistant): void => {
+                        getTranslations(ha)
+                            .then((translations: Record<string, string>) => {
+                                this._translations = translations;
+                                this._debug('translations have been retrieved. printing the translations');
+                                this._debug(this._translations);
+                            })
+                            .catch(() => {
+                                this._debug('error getting the translations');
+                            });
+                    });
             });
     }
 
-    protected async queryAttributes(detail: OnLovelaceMoreInfoDialogOpenDetail): Promise<void> {
+    protected async queryAttributes(detail: OnMoreInfoDialogOpenDetail): Promise<void> {
 
         const { HA_MORE_INFO_DIALOG_INFO } = detail;
 
@@ -187,26 +217,48 @@ class CustomMoreInfo {
 
     }
 
-    protected async queryHistoryAndLogbook(detail: OnLovelaceMoreInfoDialogOpenDetail | OnLovelaceHistoryAndLogBookDialogOpenDetail): Promise<void> {
+    protected async queryHistoryAndLogbook(detail: OnMoreInfoDialogOpenDetail | OnHistoryAndLogBookDialogOpenDetail): Promise<void> {
 
         const {
+            HA_DIALOG,
             HA_MORE_INFO_DIALOG,
             HA_DIALOG_CONTENT
         } = detail;
 
         const dialog = await HA_MORE_INFO_DIALOG.element as MoreInfoDialog;
         const entityId = await getPromisableElement(
-            () => dialog.___entry?.entity_id,
+            () => dialog.___entry?.entity_id || dialog.___entityId,
             (entityId: string): boolean => !!entityId
         );
         const domain = entityId.replace(/^(.+)\..+$/, '$1');
-        const deviceClass = dialog.___entry.original_device_class;
+        const deviceClass = dialog.___entry?.original_device_class || '';
+
+        const visibility = this.getVisibility(
+            entityId,
+            domain,
+            deviceClass
+        );
+
+        HA_DIALOG
+            .selector
+            .query(SELECTOR.MORE_INFO_HEADER)
+            .element
+            .then((header: Element): void => {
+                if (header) {
+                    this._debug('finished the task of querying the header, the result is');
+                    this._debug(header);
+                    this.addDataSelectors(header);
+                    this.hideHeaderElements(header, visibility);
+                } else {
+                    this._debug('this dialog doesn‘t have a header or it has not been found');
+                }
+            });
             
         HA_DIALOG_CONTENT
             .selector
             .deepQuery(
                 [
-                    SELECTOR.MORE_INFO_HSTORY,
+                    SELECTOR.MORE_INFO_HISTORY,
                     SELECTOR.MORE_INFO_LOGBOOK
                 ].join(',')
             )
@@ -217,14 +269,9 @@ class CustomMoreInfo {
                     const container = element.parentElement || element.getRootNode() as ShadowRoot;
                     this._debug('history or logbook have been found');
                     this._debug(element);
-                    this.hideElements(
-                        container,
-                        entityId,
-                        domain,
-                        deviceClass
-                    );
+                    this.hideContentElements(container, visibility);
                 } else {
-                    this._debug('this dialog doesn‘t have history or logbook or they have not been found');
+                    this._debug('this dialog doesn‘t have history or logbook or they have not been found.');
                 }
             });
             
@@ -257,34 +304,53 @@ class CustomMoreInfo {
         
     }
 
-    protected hideElements(
+    protected hideContentElements(
         container: Element | ShadowRoot,
-        entityId: string,
-        domain: string,
-        deviceClass: string | undefined
+        visibility: InternalVisibility
     ): void {
-
-        const visibility = this.getVisibility(
-            entityId,
-            domain,
-            deviceClass
-        );
 
         const styles = [
             visibility.hide_history
-                ? getHiddenStyle(SELECTOR.MORE_INFO_HSTORY)
+                ? getHiddenStyle(SELECTOR.MORE_INFO_HISTORY)
                 : '',
             visibility.hide_logbook
                 ? getHiddenStyle(SELECTOR.MORE_INFO_LOGBOOK)
                 : ''
         ];
 
-        if (visibility.hide_history || visibility.hide_logbook) {
+        if (
+            visibility.hide_history ||
+            visibility.hide_logbook
+        ) {
             addStyle(container, styles.join(''));
         } else {
             removeStyle(container);
         }
 
+    }
+
+    protected addDataSelectors(header: Element): void {
+        addDataSelectors(
+            header.querySelectorAll(SELECTOR.MENU_ITEM),
+            this._translations
+        );
+    }
+
+    protected hideHeaderElements(
+        content: Element,
+        visibility: InternalVisibility
+    ): void {
+
+        if (!this._translations) {
+            this._debug('skiping the header history task, because translations don‘t exist');
+            return;
+        }
+
+        if (visibility.hide_header_history_icon) {
+            addStyle(content, getHiddenStyle(SELECTOR.MORE_INFO_HEADER_HISTORY_ICON));
+        } else {
+            removeStyle(content);
+        }
     }
 
     protected getFilters(attributes: Attributes): InternalFilters {
@@ -380,48 +446,85 @@ class CustomMoreInfo {
 
         const hide = {
             history: false,
-            logbook: false
+            logbook: false,
+            header_history_icon: false
         };
 
         if (
-            this._anyGlobMatch(entityId, this._config?.hide_history?.by_glob) ||
-            this._config?.hide_history?.by_device_class?.includes(deviceClass) ||
-            this._config?.hide_history?.by_domain?.includes(domain) ||
-            this._config?.hide_history?.by_entity_id?.includes(entityId)
+            this._anyVisbilityMatch(
+                this._config?.hide_history,
+                entityId,
+                deviceClass,
+                domain
+            )
         ) {
             hide.history = true;
         }
 
         if (
-            this._anyGlobMatch(entityId, this._config?.unhide_history?.by_glob) ||
-            this._config?.unhide_history?.by_device_class?.includes(deviceClass) ||
-            this._config?.unhide_history?.by_domain?.includes(domain) ||
-            this._config?.unhide_history?.by_entity_id?.includes(entityId)
+            this._anyVisbilityMatch(
+                this._config?.unhide_history,
+                entityId,
+                deviceClass,
+                domain
+            )
         ) {
             hide.history = false;
         }
 
         if (
-            this._anyGlobMatch(entityId, this._config?.hide_logbook?.by_glob) ||
-            this._config?.hide_logbook?.by_device_class?.includes(deviceClass) ||
-            this._config?.hide_logbook?.by_domain?.includes(domain) ||
-            this._config?.hide_logbook?.by_entity_id?.includes(entityId)
+            this._anyVisbilityMatch(
+                this._config?.hide_logbook,
+                entityId,
+                deviceClass,
+                domain
+            )
         ) {
             hide.logbook = true;
         }
 
         if (
-            this._anyGlobMatch(entityId, this._config?.unhide_logbook?.by_glob) ||
-            this._config?.unhide_logbook?.by_device_class?.includes(deviceClass) ||
-            this._config?.unhide_logbook?.by_domain?.includes(domain) ||
-            this._config?.unhide_logbook?.by_entity_id?.includes(entityId)
+            this._anyVisbilityMatch(
+                this._config?.unhide_logbook,
+                entityId,
+                deviceClass,
+                domain
+            )
         ) {
             hide.logbook = false;
         }
 
+        if (
+            this._anyVisbilityMatch(
+                this._config?.hide_header_history_icon,
+                entityId,
+                deviceClass,
+                domain
+            )
+        ) {
+            hide.header_history_icon = true;
+        }
+
+        if (
+            this._anyVisbilityMatch(
+                this._config?.unhide_header_history_icon,
+                entityId,
+                deviceClass,
+                domain
+            )
+        ) {
+            hide.header_history_icon = false;
+        }
+
         this._visibility[entityId] = {
             hide_history: hide.history,
-            hide_logbook: hide.logbook
+            hide_logbook: hide.logbook,
+            hide_header_history_icon: hide.header_history_icon ||
+            (
+                !!this._config?.auto_hide_header_history_icon &&
+                hide.history &&
+                hide.logbook
+            )
         };
 
         this._debug('finished the visibility retrieval, printing the visibility...');
@@ -433,8 +536,11 @@ class CustomMoreInfo {
 
 }
 
-// Ensure the DOM is fully loaded before running the script
-Promise.resolve(customElements.whenDefined(SELECTOR.HUI_VIEW))
-	.then(() => {
-		window.customMoreInfo = new CustomMoreInfo();
-	});
+if (!window.customMoreInfo) {
+    console.info(
+        `%c  ${NAME}  \n%c  Version ${version} ${DESCRIPTION}`,
+        'color: gold; font-weight: bold; background: black',
+        'color: white; font-weight: bold; background: steelblue'
+    );
+    window.customMoreInfo = new CustomMoreInfo();
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,13 @@
 export interface CustomMoreInfoClass {
 }
 
+export interface HomeAssistant extends HTMLElement {
+	hass: {
+        language: string;
+        resources: Record<string, Record<string, string>>;
+    };
+}
+
 export enum BY_TYPES {
     by_entity_id,
     by_domain,
@@ -18,7 +25,9 @@ export type AttributeFilters = Record<
 export type ElementsVisibility = Record<
     ByTypes,
     string[]
->;
+> & {
+    all?: boolean;
+};
 
 export interface CustomMoreInfoConfig {
     debug?: boolean;
@@ -30,6 +39,9 @@ export interface CustomMoreInfoConfig {
     hide_logbook?: ElementsVisibility;
     unhide_history?: ElementsVisibility;
     unhide_logbook?: ElementsVisibility;
+    hide_header_history_icon?: ElementsVisibility;
+    unhide_header_history_icon?: ElementsVisibility;
+    auto_hide_header_history_icon?: boolean;
 }
 
 export interface InternalFilters {
@@ -40,6 +52,7 @@ export interface InternalFilters {
 export interface InternalVisibility {
     hide_history: boolean;
     hide_logbook: boolean;
+    hide_header_history_icon: boolean;
 }
 
 export interface Lovelace extends HTMLElement {
@@ -64,10 +77,11 @@ export interface Attributes extends Element {
 }
 
 export interface MoreInfoDialog extends HTMLElement {
-    ___entry: {
+    ___entry?: {
         entity_id: string;
         original_device_class?: string;
     };
+    ___entityId: string;
 }
 
 declare global {
